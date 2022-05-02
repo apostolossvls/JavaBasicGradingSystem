5186620192f9fc00e05948695a75f90c64601e2d
yannis <yannis.p18125@gmail.com>
2022-05-02 12:09:14 +0300

    changes to importStudent

--- JavaBasics/src/fileManagement/ImportStudent.java
+++ JavaBasics/src/fileManagement/ImportStudent.java
@@ -7,11 +7,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 import userManagement.Student;
-import userManagement.User;
+
 
 public class ImportStudent {
 	
-	//ενας μαθητης σε καθε γραμμη, τα χαρακτηριστηκα με τη σειρα ποθ τα λεει ο κονστρακτορ χωρισμενα με @ χωρις κενα. ο Αμ να ειναι μονο αριθμοι
+	// one student per line, userName@password@name@surname@registrationNumber, no spaces, the registrationNumber must contain only digits 
  
 	public static void  Import(String path) throws FileNotFoundException {
 		 // Creating an instance of Inputstream
@@ -20,9 +20,7 @@ public class ImportStudent {
         // Try block to check for exceptions
         try (Scanner sc = new Scanner( is, StandardCharsets.UTF_8.name())) {
  
-            // It holds true till there is single element
-            // left in the object with usage of hasNext()
-            // method
+            // It holds true till there is single element left in the object with usage of hasNext() method
             while (sc.hasNextLine()) {
             	
             	String line = sc.nextLine();
@@ -35,6 +33,8 @@ public class ImportStudent {
                      student.ShowInfo();
                 }catch(NumberFormatException e) {
                 	 System.out.println("Make sure the Registration Number cointains only Digits");
+                }catch(ArrayIndexOutOfBoundsException e) {
+                	System.out.println("Make sure the text is properly formatted");
                 }
                
               
